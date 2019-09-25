803d61de4aabaf5f4e1938995489b09d1a2918c1
Chris Maki <chris.maki@ripcitysoftware.com>
2019-09-25 09:52:04 -0700

    Upate examples in README.adoc Update comments in lambda.py

--- lambda.py
+++ lambda.py
@@ -4,6 +4,4 @@ from serverless_python_example.app import app
 
 
 def handler(event, context):
-    # app.app - the first app is the the file name, app.py,
-    #           the second app is the VARIABLE inside app.py
     return serverless_wsgi.handle_request(app, event, context)
